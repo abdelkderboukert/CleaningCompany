43cc113ec891657a12908794733505b18a8952b8
abdelkader <abdelkaderboukart@gmail.com>
2024-08-09 22:14:33 +0100

    "Added API views, configured Django settings, and updated frontend code"

--- cleaning_company/api/views.py
+++ cleaning_company/api/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from .serializers import *
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework import generics
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.request import Request
 
-# Create your views here.
+@api_view(['GET'])
+def getRoutes(request):
+    routes = [
+        '/api/token/',
+        '/api/register/',
+        '/api/token/refresh/',
+        '/api/userList/',
+        '/api/camanies/',
+        '/api/clients/',
+        '/api/buyings/',
+        '/api/balites/',
+        '/api/dattes/',
+        '/api/vers/',
+    ]
+    return Response(routes)

59c64c168dc290133263fa4daeb95bd0079b9f42
abdelkader <abdelkaderboukart@gmail.com>
2024-08-11 00:09:06 +0100

    g

---
+++ cleaning_company/frontend/src/app/component/HomePage.jsx
@@ -0,0 +1,13 @@
+export default function HomePage() {
+  return (
+    <>
+      <div className="text-red-700 bg-black h-full w-screen">HomePage</div>
+      <details
+        className=" border-2 border-black bg-black text-white rounded-xl h-48 w-60"
+        open
+      >
+        <summary>about1</summary>i want to help u
+      </details>
+    </>
+  );
+}

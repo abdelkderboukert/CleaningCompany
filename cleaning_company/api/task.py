cf0f0c31fb699c7df972c4b0d55e8ff17efee7c2
abdelkader <abdelkaderboukart@gmail.com>
2024-08-13 02:49:43 +0100

    "Updated task.py to change update field from 'hour' to 'hourjob' and updated corresponding pyc file."

--- cleaning_company/api/task.py
+++ cleaning_company/api/task.py
@@ -3,7 +3,7 @@ from api.models import *
 
 def my_task():
     print("Running my task!")
-    Employees.objects.all().update(hour=0)
+    Employees.objects.all().update(hourjob=0)
 
 scheduler = BackgroundScheduler()
 scheduler.add_job(my_task, 'cron', day='1', hour='0', minute='0')
